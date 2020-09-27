42ff07392b6e92d679b2dbdc03f2e5e59a8b1fb8
Jake Spann <jake323@comcast.net>
2020-09-27 14:30:27 -0500

    Added source folder and map parsing cpp files
    (cherry picked from commit 07922099ecd62dabe78fe06df6e1494d330d2d3f)

---
+++ Source/DrivingSchool/MapParser.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MapParser.h"
+#include "XmlParser.h"
+
+MapParser::MapParser()
+{
+    
+    fXmlFile mapFile = FXmlFile("FXmlFile");
+    UE_LOG(LogTemp, Log, TEXT("TESTING123"));
+}
+
+MapParser::~MapParser()
+{
+}
